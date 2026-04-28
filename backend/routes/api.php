81f1a30644aab6019db87934e38f933d4c32635c
Connor Buttrey <cbuttrey0216@gmail.com>
2026-04-28 09:51:32 -0500

    Updated route locations
    Moved routes and other related code to their needed spots

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,13 +1,12 @@
 <?php
 
-// api.php is only used for any frontend interactions with the database
-
 use App\Models\PrivateAssessment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 //controller connections
 use App\Http\Controllers\PrivateAssessmentController;
+use App\Http\Controllers\AssessmentController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -20,9 +19,16 @@ Route::get('/', function () {
     ]);
 });
 
-//Routes for the private assessment tabel
+
+// Routes for the private assessment tabel
 Route::get('/assessments', [PrivateAssessmentController::class, 'index']);
 Route::get('/assessments/{uuid}', [PrivateAssessmentController::class, 'show']);
 Route::post('/assessments', [PrivateAssessmentController::class, 'store']);
 Route::put('/assessments/{uuid}', [PrivateAssessmentController::class, 'update']);
 Route::delete('/assessments/{uuid}', [PrivateAssessmentController::class, 'destroy']);
+
+// Routes for lethatlity assessment
+Route::get('/portal/dashboard', [AssessmentController::class, 'index']);
+Route::get('/portal/assessment', [AssessmentController::class, 'create']);
+Route::post('/portal/assessment', [AssessmentController::class, 'store'])
+    ->middleware('throttle:20,15');
